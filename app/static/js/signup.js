e5b9101f81be7265adb703d4abfb218fcffe0247
Zheng Wang <wzheng2013@gmail.com>
2020-05-05 22:36:17 -0400

    sign up page error

--- app/static/js/signup.js
+++ app/static/js/signup.js
@@ -1,4 +1,5 @@
 function displayAuthenticationStatus() {
+    console.log("hello");
     let formStatus = document.querySelector('.form-status')
     let username = document.querySelector('#usernameInput').value
     if(username.includes('@')) {
@@ -7,15 +8,31 @@ function displayAuthenticationStatus() {
     } else if (username.includes('_')) {
         formStatus.style.display = "block"
         formStatus.textContent = "Username cannot contain _"
+    } else if ($("#emailInput").contains("@") == false) {
+        formStatus.style.display = "block"
+        formStatus.textContent = "Email not valid"
     } else {
 
-        //Ajax request here 
+        console.log($("#emailInput") + "  includes " + $("#emailInput").includes("@") + "    contains " + $("#emailInput").contains("@"));
 
-        if(data.result == "success") {
-            location.href = "/login"
-        } else {
-            formStatus.style.display = "block"
-            formStatus.textContent = data.result
-        }
+        $.ajax({
+            url: "/newUser",
+            data: {
+                username: $("#usernameInput"),
+                email: $("#emailInput"),
+                password: $("#passwordInput")
+            },
+            dataType: "JSON",
+            type: "POST",
+            success: function(data){
+                if(data.result == "success") {
+                    window.location.pathname = "/login";
+                } else {
+                    formStatus.style.display = "block"
+                    formStatus.textContent = data.result
+                }
+            }
+        });
+
     }
 }
